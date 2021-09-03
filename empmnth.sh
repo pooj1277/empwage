b8e5711589193922b1db3d6e4fb8f7b7dd846bfa
pooj1277 <pooja.sd3@gmail.com>
2021-09-03 12:14:58 -0700

    Employee Salary for month calculated

---
+++ empmnth.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Calculating Wages for a Month
+isPartTime=1;
+isFullTime=2;
+totalSalary=0;
+empRatePerHr=20;
+numWorkingDays=20;
+for (( day=1; day<=$numWorkingDays; day++ ))
+do
+	empCheck=$((RANDOM%3));
+	case $empCheck in
+		$isFullTime)
+		empHrs=8
+		echo "For Day $day The employee is working full time at wage:"
+		;;
+		$isPartTime)
+		empHrs=4
+		echo "For Day $day The employee is working part time at wage:"
+		;;
+		*)
+		empHrs=0
+		;;
+	esac
+salary=$(($empHrs*$empRatePerHr));
+echo $salary
+totalSalary=$(($totalSalary+$salary));
+echo "Monthly salary at employee hours= $empHrs is $totalSalary"
+done
